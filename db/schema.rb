5a34a4d0d3f6ee978d3f386e6fa608c7f2c5e98a
Michael Richardson <mcr@sandelman.ca>
2023-05-02 20:13:05 -0400

    fixup! d13beca9f3e7ca5e81983945a62e0a9888c16632

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_07_233245) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_24_000315) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -18,6 +18,14 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_07_233245) do
     t.text "name"
     t.boolean "active"
     t.datetime "lastused_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "dawn_boot_networkstatuses", force: :cascade do |t|
+    t.boolean "success"
+    t.boolean "inprogress"
+    t.json "testresults"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
