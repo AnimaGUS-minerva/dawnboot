a470d79edb7167d76a3f89809142f9f71c1e10e3
Michael Richardson <mcr@sandelman.ca>
2023-05-05 15:27:37 -0400

    chore: configure eager_load_paths

--- config/application.rb
+++ config/application.rb
@@ -29,8 +29,8 @@ module Dawnboot
     # in config/environments, which are processed later.
     #
     # config.time_zone = "Central Time (US & Canada)"
-    # config.eager_load_paths << Rails.root.join("extras")
 
-    config.autoload_paths += %W(#{config.root}/lib)
+    config.eager_load_paths << Rails.root.join("lib")
+    config.autoload_paths   += %W(#{config.root}/lib)
   end
 end
