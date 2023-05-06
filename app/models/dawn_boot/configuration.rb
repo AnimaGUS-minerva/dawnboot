9cbca29eef3e41d2f88be84e85802fe01486c305
Michael Richardson <mcr@sandelman.ca>
2023-05-05 20:34:47 -0400

    feat: reach out to nightclerk with email, and get an activity URL back

--- app/models/dawn_boot/configuration.rb
+++ app/models/dawn_boot/configuration.rb
@@ -1,15 +1,21 @@
+require 'net/http'
+
 module DawnBoot
   class Configuration < ApplicationRecord
     has_many :system_variables
+
+    class BadNightClerk < Exception; end
 
     def self.default
       self.where(:active => true).order("dawn_boot_configurations.lastused_at DESC").first
     end
 
     def self.create_first
-      create(:active => true,
+      cf = create(:active => true,
              :name   => "initial",
-             :lastused_at => DateTime::now).save!
+             :lastused_at => DateTime::now)
+      cf.save!
+      cf
     end
 
     def self.finddefault
@@ -20,22 +26,62 @@ module DawnBoot
       system_variables.setvalue(:admin_email, x)
     end
     def admin_email
-      lookup(:admin_email)
+      string(:admin_email)
     end
 
     def lookup(x)
       system_variables.lookup(x)
     end
+   def string(x)
+      system_variables.string(x)
+    end
+
+    def http_handler(url)
+      # open code rather than use .start(), so that debug output can be set
+      unless @http_handler
+        @http_handler = Net::HTTP.new(url.host, url.port)
+        #@http_handler.verify_mode = OpenSSL::SSL::VERIFY_NONE
+        @http_handler.use_ssl     = url.scheme == 'https'
+        #@http_handler.cert        = FountainKeys.ca.jrc_pub_key
+        #@http_handler.key         = FountainKeys.ca.jrc_priv_key
+        @http_handler.set_debug_output($stderr) if ENV['HTTP_DEBUG']
+      end
+      @http_handler
+    end
 
     def onboard!(url = lookup(:onboard_url))
-
       params = Hash.new
       params[:email] = admin_email
 
-      request = Net::HTTP::Post.new(target_uri)
-      request.body         = params.to_json
-      request.content_type = "text/json"
-      request.add_field("Accept", "text/json")
+      url = URI(url)
+      request = Net::HTTP::Post.new(url)
+      body    = params.to_json
+      request.content_type = "application/json"
+      request.add_field("Accept", "application/json")
+
+      begin
+        response = http_handler(url).request(request, body)     # Net::HTTPResponse object
+
+      rescue OpenSSL::SSL::SSLError
+        logger.error "failed to negotiate with to #{url} due to #{$!}"
+        raise BadNightClerk.new("TLS connection error: #{$!}")
+
+      rescue SocketError
+        logger.error "Failed to connect to #{url}"
+        raise BadNightClerk.new("DNS or connection error: #{$!}")
+
+      rescue
+        logger.error "Error #{$!} was raised"
+        raise $!
+      end
+
+      case response.code
+      when "201"
+        location = response.header[:location]
+        logger.info "NightClerk at #{url} says #{response.message} #{location}"
+
+        system_variables.setvalue(:enroll_activity, location)
+      end
 
     end
 
--- lib/tasks/provision.rake
+++ lib/tasks/provision.rake
@@ -4,6 +4,8 @@ namespace :dawnboot do
   desc "do initial onboarding with EMAIL=foo@example.com"
   task :onboard_with_email => :environment do
 
+    Rails.logger = Logger.new(STDOUT)
+
     cfg = DawnBoot::Configuration.finddefault
 
     email = ENV['EMAIL']
